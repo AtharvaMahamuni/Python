a219c109a3cccd637339c524caec5e94acc061ac
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2020-07-15 19:27:54 +0530

    tuple and set is added

---
+++ 01_Basics/07_tuple.py
@@ -0,0 +1,9 @@
+tuple = (12, 22, 32, 42, 52, 62, 12)
+
+print(tuple)
+
+# print(tuple.count(42))
+
+# print(tuple[0])
+
+print(tuple.index(52))
---
+++ 01_Basics/08_set.py
@@ -0,0 +1,6 @@
+set = {12, 13, 5, 64, 2, 12, 4, 5, 64}
+
+print(set)
+
+set.discard(12)
+print(set)
