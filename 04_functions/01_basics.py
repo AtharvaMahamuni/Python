795dc5de616ad1c08020e05020a8d45c05b6fbc9
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2020-07-17 21:05:46 +0530

    function bsic program

---
+++ 04_functions/01_basics.py
@@ -0,0 +1,19 @@
+
+def add(num1, num2):
+    sum = num1 + num2
+    return(sum)
+
+
+def sub(num1, num2):
+    sub = num1 - num2
+    return sub
+
+
+def greet():
+    print("Hello, World from function !!!")
+    print("I am learning functions in python")
+
+
+print(greet())
+print(add(4, 5))
+print(sub(10, 8))
