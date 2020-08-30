4a38ef7400b377b941cc353fb8d37835a18bc229
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2020-08-30 16:43:51 +0530

    __name__  special function example

---
+++ 05_modules/02__name__2.py
@@ -0,0 +1,16 @@
+import __name__
+
+def add():
+    print("from add")
+
+
+def sub():
+    print("from sub")
+
+
+def main():
+    add()
+    sub()
+
+
+main()
---
+++ 05_modules/__name__.py
@@ -0,0 +1,19 @@
+
+print("hello from ", __name__)
+
+
+def fun1():
+    print("from fun1")
+
+
+def fun2():
+    print("from fun2")
+
+
+def main():
+    fun1()
+    fun2()
+
+
+if __name__ == "__main__":
+    main()
