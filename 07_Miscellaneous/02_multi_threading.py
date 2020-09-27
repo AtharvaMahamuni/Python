002ed2b7586607e92b0e8289aff193d6ed3408e4
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2020-09-27 15:47:51 +0530

    completed course

---
+++ 07_Miscellaneous/02_multi_threading.py
@@ -0,0 +1,29 @@
+from time import sleep
+from threading import *
+
+
+class hello(Thread):
+    def run(self):
+        for i in range(0, 5):
+            print("Hello")
+            sleep(1)
+
+
+class hi(Thread):
+    def run(self):
+        for i in range(0, 5):
+            print("Hi")
+            sleep(1)
+
+
+t1 = hello()
+t2 = hi()
+
+t1.start()
+sleep(0.2)
+t2.start()
+
+t1.join()
+t2.join()
+
+print("bye")
