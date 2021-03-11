300d196065b2a316bf2d546f893769039d791f70
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2021-03-11 19:05:56 +0530

    list practice

---
+++ W3School/hello.py
@@ -0,0 +1,17 @@
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[1])
+
+print(thislist[-1])
+
+print(thislist[2:5])
+
+print(thislist[:4])
+
+print(thislist[2:])
+
+print(thislist[-4:-1])
+
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
+
