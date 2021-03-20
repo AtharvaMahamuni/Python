3403a3cc42aa1ab2880ac6b980e51c5985c4fabc
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2021-03-20 15:10:51 +0530

    college problem

---
+++ College Assignment/question_2.py
@@ -0,0 +1,22 @@
+
+num = int(input())
+
+result = []
+
+i = 10
+
+if num < 10:
+    print(-1)
+
+else:
+    while i < num:
+        num1 = i % 10
+        num2 = i // 10
+
+        if(abs(num1 - num2) == 1):
+            result.append(i)
+
+        i += 1
+
+    for i in result:
+        print(i, end=" "),
