420e50d4f66e34a904db12a16ee23a0c59c7a4c8
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2021-03-23 16:15:11 +0530

    python OOPS

--- OOPS/02.py
+++ OOPS/02.py
@@ -2,22 +2,40 @@
 class Parrot:
 
     # instance attributes
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    def __init__(self):
+        self.__name = 'blu'
 
-    # instance methods
-
-    def sings(self, song):
-        return("{} sings {}".format(self.name, song))
-
-    def dance(self):
-        return("{} is now started to dance".format(self.name))
+    def display(self):
+        print("{} is bird".format(self.__name))
 
 
-# instantiate the object
-blu = Parrot('blu', 12)
+blu = Parrot()
 
-# calling instance method
-print(blu.sings("'Happy'"))
-print(blu.dance())
+blu.display()
+
+blu.__name = "kulkanri"
+blu.display()
+
+
+#         self._age = age
+
+#     # instance methods
+
+#     def sings(self, song):
+#         return("{} sings {}".format(self._name, song))
+
+#     def dance(self):
+#         return("{} is now started to dance".format(self.name))
+
+
+# # instantiate the object
+# blu = Parrot('blu', 12)
+
+# # calling instance method
+# print(blu.sings("'Happy'"))
+# print(blu.dance())
+
+# blu._age = 15
+# blu._name = "Happy"
+
+# print(blu.sings())
--- OOPS/03.py
+++ OOPS/03.py
@@ -1,3 +1,20 @@
+class Person:
+    "This is a person class"
+    age = 10
 
-class Parrot:
-    pass
+    def greet(self):
+        print('Hello')
+        
+
+
+p = Person()
+# Output: 10
+print(Person.age)
+
+# Output: <function Person.greet>
+print(Person.greet)
+
+p.greet()
+
+# Output: "This is a person class"
+print(Person.__doc__)
