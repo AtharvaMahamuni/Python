0e408b60797df4c3e237d84c243fd9d095cb229b
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2021-03-24 12:49:27 +0530

    lambda function

---
+++ W3School/lambdaFunction.py
@@ -0,0 +1,15 @@
+
+# Using lambda function
+# x = lambda a,b : a*b
+
+# Alternative to lambda function
+def x(a, b): return a*b
+
+
+# sum = lambda num1, num2, num3 : num1 + num2 + num3
+def sum(num1, num2, num3): return num1 + num2 + num3
+
+
+print(sum(10, 20, 30))
+
+print(x(5, 6))
