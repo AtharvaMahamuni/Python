4574eedb15a2cff276a4bfdd0662891ef8db1a38
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2021-03-26 23:14:41 +0530

    Linked list practice code

--- DataStructures/practice.py
+++ DataStructures/practice.py
@@ -44,17 +44,136 @@ class LinkedList:
 
             temp.next = p
 
-    def createAtGivenLocation(self):
-        pass
+    def createAtGivenLocation(self, loc, new_data):
+
+        p = Node(new_data)
+
+        if self.head is None and loc == 1:
+            self.head = p
+
+        elif self.head is not None:
+
+            i = 1
+            q = self.head
+
+            while i < loc - 1 and q != None:
+                q = q.next
+                i += 1
+
+            if q is None:
+                print("Location is Invalid")
+
+            p.next = q.next
+            q.next = p
+
+        else:
+            print("Invalid Location")
 
     def deleteAtStart(self):
-        pass
 
-    def createAtEnd(self):
-        pass
+        if self.head is None:
+            print("Linked List is Empty.")
 
-    def createAtGivenLocation(self):
-        pass
+        else:
+            p = self.head
+            self.head = self.head.next
+            del p
+
+    def deleteAtEnd(self):
+
+        p = self.head
+
+        if self.head is None:
+            print("Linked List is Empty.")
+
+        elif self.head.next is None:
+            p = self.head
+            self.head = None
+            del p
+
+        else:
+            p = self.head.next
+            q = self.head
+
+            while p.next:
+                q = p
+                p = p.next
+
+            q.next = None
+            del p
+
+    def deleteAtGivenLocation(self, loc):
+
+        if loc == 1 and self.head is None:
+            p = self.head
+            del p
+            self.head = None
+
+        elif self.head is not None:
+            p = self.head.next
+            q = self.head
+
+            i = 1
+            while i < loc - 1 and p is not None:
+                q = p
+                p = p.next
+                i += 1
+
+            if p is None:
+                print("Invalid Location")
+
+            else:
+                q.next = p.next
+                del p
+
+        else:
+            print("Invalid Location")
 
     def traverse(self):
-        pass
+        temp = self.head
+
+        while temp:
+            print(temp.data, end=" ")
+            temp = temp.next
+        print()
+
+
+ll = LinkedList()
+
+ll.createAtStart(10)
+ll.createAtStart(20)
+ll.createAtStart(30)
+ll.createAtStart(40)
+
+ll.traverse()
+
+ll.createAtEnd(50)
+ll.createAtEnd(60)
+ll.createAtEnd(70)
+
+ll.traverse()
+
+ll.deleteAtStart()
+ll.deleteAtStart()
+ll.deleteAtStart()
+
+ll.traverse()
+
+ll.deleteAtEnd()
+ll.deleteAtEnd()
+
+ll.traverse()
+
+ll.createAtStart(10)
+ll.createAtEnd(60)
+ll.createAtEnd(70)
+ll.createAtEnd(80)
+
+ll.traverse()
+
+ll.createAtGivenLocation(3, 20)
+ll.traverse()
+
+ll.deleteAtGivenLocation(3)
+ll.deleteAtGivenLocation(1)
+ll.traverse()
