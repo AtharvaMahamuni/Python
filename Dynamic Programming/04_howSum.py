52f9cc52e6c00e530c813e2586b03b60e61225cb
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2021-04-13 18:48:36 +0530

    how sum problem final version

--- Dynamic Programming/04_howSum.py
+++ Dynamic Programming/04_howSum.py
@@ -24,24 +24,28 @@ def howSum(targetSum, numbers, memo={}):
         return None
     if targetSum in memo.keys():
         return memo[targetSum]
-    
+
     for num in numbers:
         remainder = targetSum - num
         remainderResult = howSum(remainder, numbers, memo)
         if remainderResult != None:
-            memo[targetSum] = remainderResult
             numList = [num]
-            return remainderResult + numList
+            memo[targetSum] = remainderResult + numList
+            return memo[targetSum]
+    memo[targetSum] = None
     return None
 
 
-print(howSum(7, [2, 3]))
-print(howSum(9, [2, 3, 4]))
-print(howSum(8, [2, 3, 4]))
-print(howSum(5, [2, 4]))
-print(howSum(0, [1, 2, 3]))
-print(howSum(36, [7, 6, 3, 2, 5]))
-print(howSum(300, [7, 14]))
+print(howSum(7, [2, 3], {}))
+print(howSum(9, [2, 3, 4], {}))
+print(howSum(8, [2, 3, 4], {}))
+print(howSum(5, [2, 4], {}))
+print(howSum(0, [1, 2, 3], {}))
+print(howSum(36, [7, 6, 3, 2, 5], {}))
+print(howSum(300, [7, 14], {}))
+print(howSum(300, [7, 14, 15], {}))
+
+
 # print(howSum(7, [2, 3]))
 # print(howSum(9, [2, 3, 4]))
 # print(howSum(8, [2, 3, 4]))
