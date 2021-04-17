94c84a8a6d0e9d59d84c0f312d132ea28cd2c1e8
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2021-04-17 11:41:32 +0530

    bianry tree & binary search tree

--- W3School/hello.py
+++ W3School/hello.py
@@ -1,14 +1,4 @@
-class Test:
-    def __init__(self):
-        self.a = 10
-        self.b = 20
-        self.c = 30
-        self.d = 40
 
+items = [0]
 
-t1 = Test()
-t2 = Test()
-
-del t1.a
-
-print(t2.__dict__)
+print(items[-1])
